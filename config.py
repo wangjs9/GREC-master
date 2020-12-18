8c64a90521a5d8a36f0fd7150b32d69a086e00c2
WANG Jiashuo <csjwang@msds.comp.polyu.edu.hk>
2020-12-18 15:58:28 +0800

    sota

--- config.py
+++ config.py
@@ -11,11 +11,17 @@ SEP_idx = 8
 import argparse, os, torch
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--dataset", type=str, default="empathetic")
+# parser.add_argument("--model", type=str, default="trs")
+# parser.add_argument("--model", type=str, default="cause")
+parser.add_argument("--model", type=str, default="multihop")
 
 parser.add_argument('--data_dict', type=str, default='./data/', help='name of data dictionary')
 parser.add_argument('--data_path', type=str, default='./data/dataset_preproc.p', help='name of data file')
 parser.add_argument('--data_vocab', type=str, default='./data/vocab.txt', help='name of data vocabulary file')
-parser.add_argument('--glove_path', type=str, default='/home/csjwang/Documents/glove.6B/glove.6B.300d.txt', help='name of vocab embedding file')
+parser.add_argument('--glove_path', type=str, default='../glove.6B/glove.6B.300d.txt', help='name of vocab embedding file')
+parser.add_argument("--emb_path", type=str, default="utils/embedding.txt")
+parser.add_argument("--emb_file", type=str, default="../glove.6B/glove.6B.{}d.txt")
 parser.add_argument('--save_path', type=str, default='', help='name of save file')
 parser.add_argument("--posembedding_path", type=str, default="./data/embedding_pos.txt")
 parser.add_argument('--concept_vocab', type=str, default='', help='name of concept vocabulary file')
@@ -23,67 +29,71 @@ parser.add_argument('--concept_rel', type=str, default='', help='name of concept
 parser.add_argument('--conceptnet', type=str, default='', help='name of conceptnet file')
 parser.add_argument('--conceptnet_graph', type=str, default='', help='name of conceptnet graph file')
 parser.add_argument('--triple_dict', type=str, default='', help='name of conceptnet graph file')
-# parser.add_argument('--cp_path', type=str, default='', help='name of concept path file')
-
-parser.add_argument('--dataprocess', type=bool, default=True, help='whether to process the data')
 
 parser.add_argument("--hidden_dim", type=int, default=300)
+parser.add_argument("--cause_hidden_dim", type=int, default=128)
 parser.add_argument("--emb_dim", type=int, default=300)
-parser.add_argument('--bz', type=int, default=64, help='the size of batch')
-parser.add_argument('--lr', type=int, default=32, help='learning rate')
+parser.add_argument('--bz', type=int, default=32, help='the size of batch')
+parser.add_argument('--lr', type=float, default=1e-5, help='learning rate')
+# parser.add_argument('--lr', type=float, default=1e-4, help='learning rate')
+parser.add_argument('--schedule', type=int, default=500, help='schedule step')
+parser.add_argument('--weight_decay', type=float, default=5e-6, help='weight decay rate')
 parser.add_argument('--gs', type=int, default=10000, help='total number of global steps')
-parser.add_argument("--beam_size", type=int, default=5)
-# parser.add_argument('')
+parser.add_argument("--beam_size", type=int, default=10)
 
 parser.add_argument('--pointer_gen', action="store_true")
-parser.add_argument("--weight_sharing", action="store_true")
-parser.add_argument("--label_smoothing", action="store_true")
-parser.add_argument("--noam", action="store_true")
-parser.add_argument("--universal", action="store_true")
-parser.add_argument("--emo_multitask", action="store_true")
+parser.add_argument('--is_coverage', action="store_true")
+parser.add_argument("--weight_sharing", type=bool, default=True)#action="store_true")
+parser.add_argument("--label_smoothing", type=bool, default=True)#, action="store_true")
+parser.add_argument("--noam", type=bool, default=True)#action="store_true")
+parser.add_argument("--universal", type=bool, default=True)#action="store_true")
+parser.add_argument("--emo_multitask", type=bool, default=True)#action="store_true")
 parser.add_argument("--cause_multitask", action="store_true")
-parser.add_argument("--act", action="store_true")
-parser.add_argument("--pretrain_emb", action="store_true")
+parser.add_argument("--act", type=bool, default=True)#action="store_true")
 parser.add_argument("--act_loss_weight", type=float, default=0.001)
+parser.add_argument("--pretrain_emb", type=bool, default=True)#action="store_true")
+parser.add_argument("--test", action="store_true")
+
 
 ## transformer
 parser.add_argument("--hop", type=int, default=6)
-parser.add_argument("--heads", type=int, default=1)
+parser.add_argument("--heads", type=int, default=8)
 parser.add_argument("--depth", type=int, default=40)
 parser.add_argument("--filter", type=int, default=50)
 
 ## graph
 parser.add_argument("--hop_num", type=int, default=2)
+parser.add_argument("--max_graph", type=int, default=5)
+parser.add_argument("--max_mem_size", type=int, default=400)
+parser.add_argument("--max_triple_size", type=int, default=1000)
+
+def print_opts(opts):
+    """Prints the values of all command-line arguments.
+    """
+    print('=' * 80)
+    print('Opts'.center(80))
+    print('-' * 80)
+    for key in opts.__dict__:
+        if opts.__dict__[key]:
+            print('{:>30}: {:<30}'.format(key, opts.__dict__[key]).center(80))
+    print('=' * 80)
+
 
 arg = parser.parse_args()
+print_opts(arg)
+model = arg.model
 
-dataprocess = True if arg.dataprocess else False
-
-data_dict = arg.data_dict
-data_npy_dict = arg.data_dict + 'npyFile/'
-data_concept_dict = arg.data_dict + 'concept/'
-if not os.path.exists(data_npy_dict):
-    os.mkdir(data_npy_dict)
-data_path = arg.data_path
-data_vocab = arg.data_vocab
-embed_path = arg.glove_path
-save_path = arg.save_path if arg.save_path else './save/'
-posembedding_path = arg.posembedding_path
-
-concept_vocab = arg.concept_vocab if arg.concept_vocab else '../conceptnet/concept.txt'
-concept_rel = arg.concept_rel if arg.concept_rel else '../conceptnet/relation.txt'
-conceptnet = arg.conceptnet if arg.conceptnet else '../conceptnet/concept.en.csv'
-conceptnet_graph = arg.conceptnet_graph if arg.conceptnet_graph else '../conceptnet/concept.graph'
-# cp_path = arg.cp_path if arg.cp_path else './data/paths.json'
-
-# Hyperparameters
-hidden_dim = arg.hidden_dim
+# >>>>>>>>>> hyperparameters >>>>>>>>>> #
 emb_dim = arg.emb_dim
-bz = arg.bz
-lr = arg.lr
-beam_size = arg.beam_size
-
-USE_CUDA = True if torch.cuda.is_available() else False
+hidden_dim = arg.hidden_dim
+cause_hidden_dim = arg.cause_hidden_dim
+### transformer
+hop = arg.hop
+heads = arg.heads
+depth = arg.depth
+filter = arg.filter
+hop_num = arg.hop_num
+pretrain_emb = arg.pretrain_emb
 pointer_gen = arg.pointer_gen
 label_smoothing = arg.label_smoothing
 weight_sharing = arg.weight_sharing
@@ -94,8 +104,36 @@ act_loss_weight = arg.act_loss_weight
 emo_multitask = arg.emo_multitask
 cause_multitask = arg.cause_multitask
 
-### transformer
-hop = arg.hop
-heads = arg.heads
-depth = arg.depth
-filter = arg.filter
+# >>>>>>>>>> data path >>>>>>>>>> #
+dataset = arg.dataset
+data_dict = arg.data_dict
+data_npy_dict = arg.data_dict + 'npyFile/'
+data_concept_dict = arg.data_dict + 'concept/'
+if not os.path.exists(data_npy_dict):
+    os.mkdir(data_npy_dict)
+data_path = arg.data_path
+data_vocab = arg.data_vocab
+embed_path = arg.glove_path
+emb_path = arg.emb_path
+emb_file = arg.emb_file.format(str(emb_dim))
+save_path = arg.save_path if arg.save_path else './save/{}/'.format(model)
+# save_path = arg.save_path if arg.save_path else './save/multi-{}/'.format(model)
+posembedding_path = arg.posembedding_path
+concept_vocab = arg.concept_vocab if arg.concept_vocab else '../conceptnet/concept.txt'
+concept_rel = arg.concept_rel if arg.concept_rel else '../conceptnet/relation.txt'
+conceptnet = arg.conceptnet if arg.conceptnet else '../conceptnet/concept.en.csv'
+conceptnet_graph = arg.conceptnet_graph if arg.conceptnet_graph else '../conceptnet/concept.graph'
+
+# >>>>>>>>>> training parameters >>>>>>>>>> #
+bz = arg.bz
+lr = arg.lr
+schedule = arg.schedule
+weight_decay = arg.weight_decay
+test = arg.test
+beam_size = arg.beam_size
+# device = 'cpu'
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+
+
--- main.py
+++ main.py
@@ -1,23 +1,161 @@
 import config
 import os
+import torch
+from torch.nn.init import xavier_uniform_
 from tqdm import tqdm
+import numpy as np
+from copy import deepcopy
+from tensorboardX import SummaryWriter
+from models.common_layer import evaluate, count_parameters, make_infinite
+from generation.trs import Transformer
+from generation.trs_cause import CauseLSTM
+# from generation.trs_hop import Transformer_CauseHop
+from generation.trs_multihop import MultiHopCause
+from dataprocess.data_loader import prepare_data_seq
 
-def main(ScaleType, Type):
-    if config.dataprocess:
-        from dataprocess import data_reader
-        # build_graph.save_net()
-        data_reader.load_dataset()
-        # data_reader
+def find_model_path(save_path):
+    if not os.path.exists(save_path):
+        return None
+    list = os.listdir(save_path)
+    list = [ele for ele in list if ele[:5] == 'model']
+    if list == []:
+        return None
+    list.sort(key=lambda fn: os.path.getmtime(save_path + fn))
+    # model_path = datetime.datetime.fromtimestamp(os.path.getmtime(save_path+list[-1]))
+    model_path = os.path.join(save_path, list[-1])
+    return model_path
 
-    exit()
+def train_eval(ScaleType, test=False):
+    data_loader_tra, data_loader_val, data_loader_tst, vocab, program_number = prepare_data_seq(
+        batch_size=config.bz)
+    # exit()
+    torch.manual_seed(0)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(0)
 
+    model_file_path = find_model_path(config.save_path)
 
-    os.makedirs(config.save_path)
-    bar = tqdm(range(config.gs), total=config.gs)
+    if config.test:
+    # if test:
+        print('Test model', config.model)
+        if config.model == "trs":
+            model = Transformer(vocab, decoder_number=program_number, model_file_path=model_file_path)
+        elif config.model == 'cause':
+            model = CauseLSTM(vocab, decoder_number=program_number, model_file_path=model_file_path)
+        elif config.model == 'multihop':
+            model = MultiHopCause(vocab, decoder_number=program_number, model_file_path=model_file_path, load_optim=True)
+        model.to(config.device)
+        evaluate(model, data_loader_tst, ty="test", max_dec_step=50)
+        exit(0)
 
-    for step in bar:
-        pass
+    if config.model == 'trs':
+        model = Transformer(vocab, decoder_number=program_number, model_file_path=model_file_path)
+        if model_file_path is None:
+            for n, p in model.named_parameters():
+                if p.dim() > 1 and n != "embedding.lut.weight" and config.pretrain_emb:
+                    xavier_uniform_(p)
+
+    elif config.model == 'cause':
+        model = CauseLSTM(vocab, decoder_number=program_number, model_file_path=model_file_path)
+        if model_file_path is None:
+            for n, p in model.named_parameters():
+                if p.dim() > 1 and n != "embedding.lut.weight" and config.pretrain_emb:
+                    xavier_uniform_(p)
+
+    elif config.model == 'multihop':
+        # model = Transformer_CauseHop(vocab, decoder_number=program_number, model_file_path=model_file_path)
+        model = MultiHopCause(vocab, decoder_number=program_number, model_file_path=model_file_path)
+        if model_file_path is None:
+            for n, p in model.named_parameters():
+                if p.dim() > 1 and n !="embedding.lut.weight" and config.pretrain_emb:
+                    xavier_uniform_(p)
+
+    print('MODEL USED', config.model)
+    print('TRAINABLE PARAMETERS', count_parameters(model))
+
+    check_iter = 1000
+
+    try:
+        model.to(config.device)
+        init_iter = model.iter + 1
+        best_ppl = model.current_loss
+        patient = 0
+        writer = SummaryWriter(log_dir=config.save_path)
+        weights_best = deepcopy(model.state_dict())
+        data_iter = make_infinite(data_loader_tra)
+        model = model.train()
+        for n_iter in tqdm(range(100000)):
+            loss, ppl, bce, acc = model.train_one_batch(next(data_iter))
+            writer.add_scalars('loss', {'loss_train': loss}, n_iter+init_iter)
+            writer.add_scalars('ppl', {'ppl_train': ppl}, n_iter+init_iter)
+            if type(bce) == tuple:
+                writer.add_scalars('bce_1', {'bce_emo_train': bce[0]}, n_iter+init_iter)
+                writer.add_scalars('bce_2', {'bce_cause_train': bce[1]}, n_iter+init_iter)
+            else:
+                writer.add_scalars('bce', {'bce_train': bce}, n_iter+init_iter)
+            if type(acc) == tuple:
+                writer.add_scalars('accuracy_emo', {'acc_emo_train': acc[0]}, n_iter+init_iter)
+                writer.add_scalars('accuracy_cause', {'acc_cause_train': acc[1]}, n_iter+init_iter)
+            else:
+                writer.add_scalars('accuracy', {'acc_train': acc}, n_iter+init_iter)
+            if (config.noam):
+                writer.add_scalars('lr', {'learning_rata': model.scheduler._rate}, n_iter+init_iter)
+
+            if (n_iter + 1) % check_iter == 0:
+                model = model.eval()
+                loss_val, ppl_val, bce_val, acc_val, bleu_score_g, bleu_score_b = evaluate(model, data_loader_val,
+                                                                                           ty="valid", max_dec_step=50)
+                model = model.train()
+                writer.add_scalars('loss', {'loss_valid': loss_val}, n_iter+init_iter)
+                writer.add_scalars('ppl', {'ppl_valid': ppl_val}, n_iter+init_iter)
+                if type(bce_val) == tuple:
+                    writer.add_scalars('bce_emo', {'bce_emo_valid': bce_val[0]}, n_iter+init_iter)
+                    writer.add_scalars('bce_cause', {'bce_cause_valid': bce_val[1]}, n_iter+init_iter)
+                else:
+                    writer.add_scalars('bce', {'bce_valid': bce_val}, n_iter+init_iter)
+                if type(acc_val) == tuple:
+                    writer.add_scalars('accuracy_emo', {'acc_valid': acc_val[0]}, n_iter+init_iter)
+                    writer.add_scalars('accuracy_cause', {'acc_valid': acc_val[1]}, n_iter+init_iter)
+                else:
+                    writer.add_scalars('accuracy', {'acc_valid': acc_val}, n_iter+init_iter)
+
+                if n_iter + init_iter < 2600:
+                    continue
+
+                if ppl_val <= best_ppl:
+                    best_ppl = ppl_val
+                    patient = 0
+                    model.save_model(best_ppl, n_iter+init_iter, 0, 0, bleu_score_g, bleu_score_b)
+                    weights_best = deepcopy(model.state_dict())
+                else:
+                    patient += 1
+                if patient > 2:
+                    break
+
+    except KeyboardInterrupt:
+        print('-' * 89)
+        print('Exiting from training early')
+    if test:
+        model.load_state_dict({name: weights_best[name] for name in weights_best})
+        model.eval()
+        model.epoch = 100
+        loss_test, ppl_test, bce_test, acc_test, bleu_score_g, bleu_score_b = evaluate(model, data_loader_tst,
+                                                                                       ty="test", max_dec_step=50)
+
+        file_summary = config.save_path + "summary.txt"
+        with open(file_summary, 'w') as the_file:
+            if type(acc_test) == tuple:
+                the_file.write("EVAL\tLoss\tPPL\tAccuracy_emo\tAccuracy_cause\tBleu_g\tBleu_b\n")
+                the_file.write(
+                    "{}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.2f}\t{:.2f}\n".format("test", loss_test, ppl_test, acc_test[0], acc_test[1],
+                                                                          bleu_score_g, bleu_score_b))
+            else:
+                the_file.write("EVAL\tLoss\tPPL\tAccuracy\tBleu_g\tBleu_b\n")
+                the_file.write("{}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.2f}\t{:.2f}\n".format("test", loss_test, ppl_test, acc_test,
+                                                                                 bleu_score_g, bleu_score_b))
+
 
 
 if __name__ == '__main__':
-    main('min', 'train')
+    train_eval('min', True)
